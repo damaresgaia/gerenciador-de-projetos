2a02bc609bc469495bdde141789dd066e286f778
Damares Gaia <damaresgaia@gmail.com>
2024-12-23 09:19:23 -0300

    adiciona estrutura básica do dashboard com sidebar e navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,10 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import DashboardWrapper from "./dashboardWrapper";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
+const inter = Inter({ subsets: ["latin"] });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+        className={inter.className}>
+          <DashboardWrapper>{children}</DashboardWrapper>
       </body>
     </html>
   );
